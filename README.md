b356e1144a8174accbc0862d723d7b973edcec2c
cmartinjr2008 <cmartinjr2008@gmail.com>
2024-04-19 11:26:53 -0700

    continued project work, fixed letter splitter in letter split function.

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-This is an expansion on the bookbot program it should:
-Take any .txt file that is fed to it and parse out how
-many letters, words, and special characters in the document. 
-
-This needs to be quick so that it can run on pretty much any
-system. 
+#-----------------------------------------------------------------------------
+# Description:
+#   This program is an expansion on the bookbot guided project from boot.dev.
+#   it is designed to accept any .txt document and analyze its contents. This is
+#   more or less just a project I'm using to figure out how python works. 
+#
+# Method:
+#  The program really justs loops through a given list multiple times at different points
+#  and is as granular as the user wants it to be. 
+#
+# Owner?: Onix
+#
+# Code Description:
+#   Language: Python 13.12.2
+#   
+#-----------------------------------------------------------------------------
--- main.py
+++ main.py
@@ -1,65 +1,4 @@
-def breakingBookApart(book):
-     splitBook = book.split()
-     return splitbook
-
-def letterToLower(splitbook):
-   for words in splitbook:
-        if words.isalpha():
-            for letter in words:
-                lettersplit += letter.lower()
-
-def letterCount(splitBook):
-    letterCount = {}    
-    #for x in range(len(splitBook)): add to lower if user choses for letter in words: lettersplit += letter.lower()
-    for letter in splitBook:
-        if letter in letterCount.keys():
-            letterCount[letter] += 1
-                    
-        else:
-            letterCount[letter] = 1
-                              
-    for letter in letterCount:
-        print("The", letter, "character was found ", letterCount[letter], "times")
-
-def wordCount(splitBook):
-     numberOfWords = 0
-     for words in splitBook:
-        numberOfWords += 1
-    
-def killBookBot():
-    print("Well be seeing ya!: ")
-    exit()
-
-def inputVerify(choice,filterType):
-    verC = choice.lower()
-    print(verC)
-    print(choice)
-    verified = False
-    
-    while verified == False:    
-        if (filterType == "yesNoQuestion"):
-            if verC == 'yes' or verC == 'n' or verC == 'no' or verC == 'quit':
-                verified = True
-            else:
-                verified = False
-        
-        elif(filterType == "bookCase"):
-            if verC == 'book' or verC == 'letters' or verC == 'both' or verC == 'quit':
-                verified = True
-            else:
-                verified = False
-
-        else:
-            print("Sorry I didnt quite catch that, please try again!")
-            verified = False
-    return verC
-    
-
-with open('books/frankenstein copy.txt') as f:
-    bookFile = f.read()
-    splitbook = []
-    lettersplit = []
-     
+def welcomTxt():
     print("                       _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _                                                ")
     print("       ! ! ! ! !     /  _    _   _____   _       _   ____   _   _  _   \\")
     print("       | O   O |    /  | |__| | |  _  | | |     | | |  _ \\ | |_| || |   \\")
@@ -71,55 +10,144 @@ with open('books/frankenstein copy.txt') as f:
     print("| ----Uploaded Book------ |")
     print("| -----Hold Please . . .- |")
     print("| _______________________ |")
-    print("                         ")
-    print("                         ")
-    splitbook = breakingBookApart(bookFile)
+    print("                           ")
+    print("                           ")
+
+def uploadTxtFile():
+    with open('books/frankenstein copy.txt') as text:
+        return text.read()
+
+def letterToLower(lowerLetter):
+    shiftedDoc = lowerLetter
+    for i in range(0, len(shiftedDoc)):
+            if (shiftedDoc[i].isalpha()):
+                shiftedDoc[i] = shiftedDoc[i].lower()  
+            
+    return lowerLetter
+
+#count how many characters are in each word of the txt file. 
+def characterCount(splitBook):
+    letterCount = {}
+    wordExploder = []
+    
+    for word in splitBook: #loop through the string thats already been split
+        for letter in word: #loop though each word and split
+            if letter in letterCount.keys(): #Idefify if a ltter has a key postion if not add it to the list
+                letterCount[letter] += 1 
+            else:
+                letterCount[letter] = 1
+
+    for letter in letterCount:
+        print(f"The {letter} character was found {letterCount[letter]} times")
+
+#count how many words we have in the txt file
+def wordCount(splitBook):  
+    wordCount= {}
+    numberOfWords = 0
+    for words in splitBook:
+        numberOfWords += 1
+    
+    for word in splitBook:
+        if word in wordCount.keys():
+            wordCount[word] += 1            
+        else:
+            wordCount[word] = 1
+                              
+    for word in wordCount:
+        print(f"{word} was found a total of {wordCount[word]} times")
+
+#Function to count and split letters.  
+def comboCount(splitBook):
+    letterCount = {}
+    wordCount= {}    
+    #for x in range(len(splitBook)): add to lower if user choses for letter in words: lettersplit += letter.lower()
+    numberOfWords = 0
+    for words in splitBook:
+        numberOfWords += 1
+    
+    print(splitBook)
+    for letter in splitBook:
+        if letter in letterCount.keys():
+            letterCount[letter] += 1
+                    
+        else:
+            letterCount[letter] = 1
+
+       
+    numberOfWords = 0
+    for words in splitBook:
+        numberOfWords += 1
+    
+    for word in splitBook:
+        if word in wordCount.keys():
+            wordCount[word] += 1            
+        else:
+            wordCount[word] = 1
+                              
+    for word in wordCount:
+        print(f"{word} was found a total of {wordCount[word]} times")
+
+def killBookBot():
+    print("Well be seeing ya!: ")
+    exit()
+
+def inputVerify(choice,filterType):
+    verC = choice.lower()
+    print(verC)
+    verified = False
+    
+    while verified == False:    
+        if (filterType == "yesNoQuestion"):
+            if verC == 'yes' or verC == 'no' or verC == 'quit':
+                verified = True 
+            else:           
+                print("Sorry I didnt quite catch that, please try again!")
+                verC = None
+                verified = False
+                verC = input("Please enter Yes or No: ")
+        
+        elif(filterType == "bookCase"):
+            if verC == 'words' or verC == 'letters' or verC == 'both' or verC == 'quit':
+                verified = True
+            else:
+                print("Sorry I didnt quite catch that, please try again!")
+                verified = False
+                verC = input("Please enter Book, Letter, or Both: ")
+    return verC
+
+def main():
+    txtDoc = []  
+    welcomTxt()
+    bookFile = uploadTxtFile()
+    txtDoc = bookFile.split()
+    print (txtDoc)
 
     print("Do you want to shift everything to lowercase?")
     verC = input("Type Yes or No: ")
-    toLower = False
     verC = inputVerify(verC,"yesNoQuestion")
-    if verC == 'yes':
-        toLower = True
+    
+    if(verC == "yes"):
+        verC = None
+        letterToLower(txtDoc)
+                       
     else:
-        toLower = False
-    verC = None
-    
+        verC = None
+
+    print("\n")
     print("Great! Now do you want a report on letters, words or both?")
-    verC = inputVerify()
+    verC = input("Enter Words, Letters, or Both: ")
+    verC = inputVerify(verC, "bookCase")
     
+    if(verC == "words"):
+        wordCount(txtDoc)
+   
+    elif(verC == "letters"):
+        characterCount(txtDoc)
+   
+    elif(verC == "both"):
+        wordCount(txtDoc)
+        characterCount(txtDoc)
        
 
-
-    
-   
-
-
-    print(toLower)
-
-
-
-
-        
-        
-         
-    
-   
-
-    
-
-    letterToLower(splitbook)
-    
-
-
-        
-
-
-    
-
-    print("There are a total of:", "words in this book")
-    print("_________________________________")
-    print("Here is the breakdown of letters: ")
-    print("_________________________________")
-    
-    letterCount(lettersplit)
+if __name__ == "__main__":
+    main()
